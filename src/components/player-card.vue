2d14eaabae206009ecc2a686e691d2251039e31c
BlackSpel <43000402+BlackSpel@users.noreply.github.com>
2020-02-22 15:17:51 +0300

    Изменил верстку

---
+++ src/components/player-card.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div v-for="player in players" :key="player.steamid">
+      <md-card class="md-primary">
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">{{ player.personaname }}</div>
+            <div class="md-subhead">
+              <table class="table-player-info">
+                <tr>
+                  <th>Steam ID</th>
+                  <th>Team</th>
+                  <th>Country code</th>
+                  <th>Profile url</th>
+                </tr>
+                <tr>
+                  <td width="20%">{{ player.steamid }}</td>
+                  <td width="20%">{{ player.team_name }}</td>
+                  <td width="20%">{{ player.loccountrycode }}</td>
+                  <td width="40%"><a :href="player.profileurl" target="_blank">{{ player.profileurl }}</a></td>
+                </tr>
+              </table>
+            </div>
+          </md-card-header-text>
+
+          <md-card-media>
+            <md-avatar class="md-large">
+              <img :src="player.avatarfull" alt="Avatar">
+            </md-avatar>
+          </md-card-media>
+        </md-card-header>
+
+        <md-card-actions>
+          <md-button>Matches list</md-button>
+        </md-card-actions>
+      </md-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlayerCard',
+  props: {
+    players: Array
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import url('./player-card-style.scss');
+</style>
